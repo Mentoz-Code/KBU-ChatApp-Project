7ec0aa23ea6f727f3fbdc6433df8babce779a801
Mentoz-Code <anlhassoun@gmail.com>
2024-11-23 22:30:06 +0300

    improved user search
    made it more dynamic

--- js/users.js
+++ js/users.js
@@ -37,3 +37,19 @@ searchBar.onkeyup = ()=>{
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("searchTerm=" + searchTerm);
 }
+
+//every second
+setInterval(() =>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "php/users2.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            let data = xhr.response;
+            //dynamic user refresh
+            if(!searchBar.classList.contains("active")) {
+                usersList.innerHTML = data;
+            }
+        }
+    }
+    xhr.send();
+}, 1000)
